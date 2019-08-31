0c40ca91cff7ba19de1e6b121e618342581f36f3
xiaoman0220 <39756603+xiaoman0220@users.noreply.github.com>
2019-08-31 22:28:28 +0800

    Update app_main.py

--- src/app_main.py
+++ src/app_main.py
@@ -37,7 +37,7 @@ REC_INTENTS = ['RecommendationIntent','TravellerTypeIntent', 'CategoryIntent', '
 INFO_ENTITIES = ['introduction','something', 'open', 'address', 'duration']
 
 #Google Maps API KEYS#
-APIKEY = 'AIzaSyAgU9a5eTrwZP9pIb0eNuNRu3iPE75tR-8'
+APIKEY = {APIKEY}
 
 #Load database#
  
